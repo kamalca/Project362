fd1a9eef6181c391f84ba5088af9621026454268
kamalca <sonca1999@outlook.com>
2018-03-03 15:30:17 -0800

    new file

---
+++ Project362/DataManager.swift
@@ -0,0 +1,12 @@
+//
+//  DataManager.swift
+//  Project362
+//
+//  Created by Kameron Carr on 3/3/18.
+//  Copyright © 2018 Kameron Carr. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+
